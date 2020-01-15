db3c4c8f69d93b1bb4e79b5fe883f149148c266f
gcdev373 <gcdev373@users.noreply.github.com>
2020-01-15 17:13:55 +0000

    Fixed URL

--- demonstrator/bash/sendToStroom.sh
+++ demonstrator/bash/sendToStroom.sh
@@ -2,9 +2,9 @@
 
 SUFFIX='.txt'
 
-for FILE in "ls $SUFFIX"; do
+for FILE in `ls *$SUFFIX`; do
   FEED=`basename -s $SUFFIX $FILE`
-  curl -k --data-binary @${FILE} "https://localhost/stroom/datafeed" -H "Feed:${FEED}"
+  curl -k --data-binary @${FILE} "http://localhost:8080/stroom/noauth/datafeed" -H "Feed:${FEED}"
 # http http://localhost:8080/stroom/datafeed @${FILE} Feed:${FEED}
 done
 
