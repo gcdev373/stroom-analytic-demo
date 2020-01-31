426ddfd54d261d8ab67e0e4ed4d27e6b66847a1c
gcdev373 <gcdev373@users.noreply.github.com>
2020-01-31 17:40:38 +0000

    Remove dead code

--- demonstrator/analytics/python/singleEventComplexAnalysis.py
+++ demonstrator/analytics/python/singleEventComplexAnalysis.py
@@ -37,14 +37,6 @@ df = spark \
     .option("includeHeaders", "true") \
     .load()
 
-# df = spark.read.format('stroom.spark.datasource.StroomDataSource').load(
-#         token=os.environ['STROOM_API_KEY'],host='localhost',protocol='http',
-#         uri='api/stroom-index/v2',traceLevel="0",
-#         index='5b41ebbf-b53e-41e6-a4e5-e5a220d8fd69',pipeline='13143179-b494-4146-ac4b-9a6010cada89',
-#         maxResults='300000').filter((col('idxEventTime') > '2019-12-01T00:00:00.000Z')
-#             & (col('idxEventTime') < '2020-01-01T00:00:00.000Z')
-#             & (col('idxDescription') == 'Authentication Failure'))
-
 def timestamp_now (dummy):
     return datetime.datetime.utcnow().isoformat()[:-3]+'Z'
 now_udf = udf(timestamp_now, StringType())
@@ -54,7 +46,7 @@ def event_ref (streamId, eventId):
 eventref_udf = udf(event_ref, StringType())
 
 def description (hour, count, prediction):
-    return 'Unexpectedly High Number of Authentication Failures Rate, during hour starting ' + hour \
+    return 'Unexpectedly High Number of Authentication Failures during hour starting ' + hour \
            + '.  Expected count:' + str(int(prediction)) + '. Actual count:' + str(count)
 
 description_udf = udf(description, StringType())
@@ -92,34 +84,19 @@ wideDf = df.withColumn('json',col('value').cast('string')). \
             hour("timestamp").alias("hour")).agg(collect_set(eventref_udf('streamid','eventid'))
                                                  .alias('eventref')).alias("wide")
 
-# print("Wide")
-# wideDf.show()
-
 
 pipelineModel = PipelineModel.load("jupyter/singleEventComplexAnalysis/models/inputVecPipelineModel")
 
 featuresDf = pipelineModel.transform(wideDf)
 
-# print("Features")
-# featuresDf.show()
-
 vectorAssembler = VectorAssembler(inputCols = ['hourVec','dayVec'], outputCol = 'features')
 
 fullDf = vectorAssembler.transform(featuresDf)
-#.select('window','features',size('eventref'))
-
-# print("Full")
-# fullDf.show()
 
 lrModel = LogisticRegressionModel.load("jupyter/singleEventComplexAnalysis/models/logisticRegressionAuthFailuresModel")
 
 lrDf = lrModel.transform (fullDf).alias("transformed")
 
-# print("LR")
-# lrDf.show()
-
-# #outputMode can be append, complete or update
-#join(wideDf, col("wide.window") == col('transformed.window'), 'inner').writeStream. \
 query = lrDf.writeStream. \
     outputMode("update"). \
     foreachBatch (process_batch). \
