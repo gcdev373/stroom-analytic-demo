ae42f45a4d4cbb0df855dc591537c4b4b18d67ec
gcdev373 <gcdev373@users.noreply.github.com>
2020-02-03 08:33:22 +0000

    Rename python file

--- demonstrator/bash/startDetectUnexpectAuthFailures.sh
+++ demonstrator/bash/startDetectUnexpectAuthFailures.sh
@@ -13,4 +13,4 @@ export SPARK_MAJOR_VERSION=2
 
 cd ../analytics
 
-$SPARK_HOME/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.3,org.apache.kafka:kafka-clients:0.10.0.1  python/singleEventComplexAnalysis.py
+$SPARK_HOME/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.3,org.apache.kafka:kafka-clients:0.10.0.1  python/detectUnexpectedAuthFailures.py
