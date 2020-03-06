b206ed1875c309cc704521886c5aa96a9edf9e40
gcdev373 <gcdev373@users.noreply.github.com>
2020-03-06 15:44:11 +0000

    Increase console output

--- demonstrator/bash/sendToStroom.sh
+++ demonstrator/bash/sendToStroom.sh
@@ -3,10 +3,14 @@
 SUFFIX='.txt'
 
 echo "This script expects to be run from the directory containing the .txt files you wish to send"
+echo
 sleep 3
 for FILE in `ls *$SUFFIX`; do
   FEED=`basename -s $SUFFIX $FILE`
-  curl -k --data-binary @${FILE} "http://localhost:8080/stroom/noauth/datafeed" -H "Feed:${FEED}"
+  RESPONSE=`curl -w '%{http_code}' -k --data-binary @${FILE} "http://localhost:8080/stroom/noauth/datafeed" -H "Feed:$FEED"`
+
+  echo Sent $FILE to Stroom feed $FEED with response code $RESPONSE
+#  curl -k --data-binary @${FILE} "http://localhost:8080/stroom/noauth/datafeed" -H "Feed:${FEED}"
 # http http://localhost:8080/stroom/datafeed @${FILE} Feed:${FEED}
 done
 
